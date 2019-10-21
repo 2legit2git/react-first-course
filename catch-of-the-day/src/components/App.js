8ecd18a0836158f11e9b6fd475ed2f6cda94d4af
phil <phil@nxsmotorsports.com>
2019-10-20 23:30:06 -0400

    continued progress

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React from "react";
 
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import Fish from "./Fish";
+
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
- render() {
-   return (
-     <div className="catch-of-the-day">
-       <div className="menu">
-         <Header tagline="Fresh Seafood Market" />
-       </div>
-       <Order />
-       <Inventory />
-     </div>
-   )
- }
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = key => {
+    const order = { ...this.state.order };
+
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory
+          loadSampleFishes={this.loadSampleFishes}
+          addFish={this.addFish}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
